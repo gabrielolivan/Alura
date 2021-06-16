f3818814653cc6e22dc1336d0fe7d65d76427f32
Gabriel Olivan <gabrielolivan@users.noreply.github.com>
2021-06-16 00:18:51 -0300

    up

--- Python/Python_3_parte_1_introducao a nova versao da linguagem/jogos/forca.py
+++ Python/Python_3_parte_1_introducao a nova versao da linguagem/jogos/forca.py
@@ -4,11 +4,28 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    letras_faltando = str(letras_acertadas.count('_'))
+
     enforcou = False
     acertou = False
 
+    print(letras_acertadas)
+
     while (not enforcou) and (not acertou):
-        print("Jogando...")
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+
+            if chute.upper() == letra.upper():
+                letras_acertadas[index] = letra
+
+            index += 1
+
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
