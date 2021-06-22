cecac52678bcea0ab1267ccb1a3b2f75697e2856
Gabriel Olivan <gabrielolivan@users.noreply.github.com>
2021-06-22 19:36:04 -0300

    end extrator_url.py

--- Python/extrator-url/main.py
+++ Python/extrator-url/main.py
@@ -1,8 +1,28 @@
-url = 'bytebank.com/cambio?moedaOrigem=real'
-print(url)
+# url = 'https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real'
+url = ' '
 
-url_base = url[0:19]
-print(url_base)
+# Sanitização da URL
+url = url.strip()
 
-url_parametro = url[20:36]
-print(url_parametro)
+# Validação da URL
+if url == '':
+    raise ValueError('A URL está vazia')
+
+# Separa base e os parametros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+# Busca o valor d eum parâmetro
+parametro_busca = "quantidade"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
+
