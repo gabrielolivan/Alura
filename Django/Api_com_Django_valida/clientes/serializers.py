e69a997f879729da822ccf905ace4419f7bd3a4a
GabrielOlivan <gabrielolivan@gmail.com>
2023-03-11 12:10:32 -0300

    finalizado o curso de api rest django 2

--- Django/Api_com_Django_valida/clientes/serializers.py
+++ Django/Api_com_Django_valida/clientes/serializers.py
@@ -8,11 +8,11 @@ class ClienteSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf': "O CPF deve ter 11 dígitos"})
+            raise serializers.ValidationError({'cpf': "O número de CPF inválido"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome':"Não incluia números neste campo"})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
         if not celular_valido(data['celular']):
-            raise serializers.ValidationError({'celular':"O celular deve ter 11 dígitos"})
+            raise serializers.ValidationError({'celular':"O celular deve seguir este modelo: 11 91234-1234 (respeitando os espaços e traços)"})
         return data
